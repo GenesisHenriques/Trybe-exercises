e9d01672949aceb8e4cfa0add566a3ff15db9105
GenesisHenriques <genesishenriques7@gmail.com>
2021-05-24 18:24:37 -0300

    exercises dia 4.3

---
+++ bloco_4/dia_3/exercises/01.js
@@ -0,0 +1,10 @@
+let num = 50;
+let nums = [];
+for (let contUm = 0; contUm < num; contUm += 1){
+  nums.push('*');
+}
+let a = nums.join(" ");
+
+for(let contDois = 0; contDois < num; contDois += 1){
+  console.log(a);
+}

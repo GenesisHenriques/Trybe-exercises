313cc16cb00f03250e2760af45c86046fcbc591f
GenesisHenriques <genesishenriques7@gmail.com>
2021-07-06 15:23:12 -0300

    finalizando dia 8.5

--- bloco_6/dia_1/script.js
+++ bloco_6/dia_1/script.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+
   const nomes = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espirito Santo',   'Goiás', 'Maranhão', 'Mato Grosso do Sul', 'Mato Grosso', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
   const estados = document.getElementById('estados');

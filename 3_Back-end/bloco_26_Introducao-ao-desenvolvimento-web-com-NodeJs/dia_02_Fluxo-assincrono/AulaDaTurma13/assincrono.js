608dde6a21af187df85a2ade5bf552b2d0e5b308
GenesisHenriques <genesishenriques7@gmail.com>
2021-12-02 00:34:59 -0300

    .

---
+++ 3_Back-end/bloco_26_Introducao-ao-desenvolvimento-web-com-NodeJs/dia_02_Fluxo-assincrono/AulaDaTurma13/assincrono.js
@@ -0,0 +1,12 @@
+const fs = require('fs'); // "Baixa" o fs
+
+const nomeDoArquivo = 'meu-arquivo.txt';
+
+fs.readFile(nomeDoArquivo, 'utf8')
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(`Não foi possível ler o arquivo ${nomeDoArquivo}\n Erro: ${err}`);
+    process.exit(1); // Encerra a execução do script e informa ao sistema que houve um erro com o código
+  });

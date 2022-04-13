da6b4cc7016f3e23051424f7a0f3ef1b0b473bce
GenesisHenriques <genesishenriques7@gmail.com>
2022-04-13 15:06:50 -0300

    Finalziando conteudo do course

---
+++ 4_Ciencia-da-computacao/bloco_35_Redes-e-raspagem-de-dados/dia_03_Raspagem-de-dados/Course/banco_de_dados.py
@@ -0,0 +1,20 @@
+from pymongo import MongoClient
+
+# Por padrão o host é localhost e porta 27017
+# Estes valores podem ser modificados passando uma URI
+# client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient()
+db = client.catalogue
+documents = [
+    {
+        "title": "A Light in the Attic",
+    },
+    {
+        "title": "Tipping the Velvet",
+    },
+    {
+        "title": "Soumission",
+    },
+]
+db.books.insert_many(documents)
+client.close()  # fecha a conexão com o banco de dados

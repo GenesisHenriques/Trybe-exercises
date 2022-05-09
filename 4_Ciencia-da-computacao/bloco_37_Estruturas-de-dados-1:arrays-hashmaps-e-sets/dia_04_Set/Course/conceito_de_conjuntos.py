898bac6bd6b66477f324226702b94f54907182c5
GenesisHenriques <genesishenriques7@gmail.com>
2022-05-09 12:31:00 -0300

    Finalizando dia 37.4

---
+++ 4_Ciencia-da-computacao/bloco_37_Estruturas-de-dados-1:arrays-hashmaps-e-sets/dia_04_Set/Course/conceito_de_conjuntos.py
@@ -0,0 +1,8 @@
+# Listagem explícita:
+# A = {1, 2, 3, 4, 5, 6}
+
+# Descrição dos elementos
+# B = {x | x é um número inteiro tal que 0 < x =< 6}
+# Ou seja, x, onde x é um número inteiro tal que x menor igual a 6 e maior que
+# zero. Ou seja, faz parte desse conjunto números maiores que 0 e menores
+# iguais a 6 ({1, 2, 3, 4, 5, 6}).
---
+++ 4_Ciencia-da-computacao/bloco_37_Estruturas-de-dados-1:arrays-hashmaps-e-sets/dia_04_Set/Course/formas_de_representar_conjunto.py
@@ -0,0 +1,14 @@
+# Array
+
+A = [False for i in range(5)]
+A[1] = True
+A[2] = True
+A[3] = True
+print(A)
+
+
+# Dict
+B = {"teste": True, 1: True, 2: True, 3: True}
+print(1 in B)
+print(4 in B)
+print("teste" in B)
